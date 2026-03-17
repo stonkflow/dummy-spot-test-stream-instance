9f6005459e8125b09c045fb1f9a0256099f28381
Mikhail Gerasimov <migerasimoff@gmail.com>
2026-03-17 22:36:52 +0100

    chore: options dummy

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
-	arg "github.com/alexflint/go-arg"
+	"github.com/alexflint/go-arg"
 )
 
 var options struct {
-	Workers int `arg:"--count,env:NUM_WORKERS"`
+	SourceStreams   string `arg:"--source-streams,env:SOURCE_STREAMS,required" help:"Список стримов источника (CSV), например: btcusdt@trade,btcusdt@depth@100ms."`
+	SourceUri       string `arg:"--source-uri,env:SOURCE_URI" help:"WebSocket URI источника рыночных данных."`
+	TopicCommand    string `arg:"--topic-command,env:KAFKA_TOPIC_COMMAND_IN" help:"Kafka topic для входящих команд управления сервисом."`
+	TopicEventTrade string `arg:"--topic-event-trades,env:KAFKA_TOPIC_TRADES_OUT" help:"Kafka topic для исходящих trade-событий."`
+	TopicEventDepth string `arg:"--topic-event-depth,env:KAFKA_TOPIC_DEPTH_OUT" help:"Kafka topic для исходящих depth-событий."`
 }
 
 func main() {
 	arg.MustParse(&options)
-	fmt.Println("Workers:", options.Workers)
+	fmt.Println("STREAMS:", options.SourceStreams)
 }
